9beaf01ade8d97064428daa2fdc1f55b8c2a2f82
Jakub Korsak <korsakuba@gmail.com>
2017-08-09 14:35:10 +0200

    getRoot

--- src/CalculateRoots.java
+++ src/CalculateRoots.java
@@ -12,10 +12,27 @@ public class CalculateRoots {
         int degree = getDegree();
         List<Double> factors = getFactors(degree);
 
-        printResult(evaluate(factors, -2.0), -2.0);
+        System.out.println("Root: " + getRoot(factors, degree));
+
 
     }
 
+    private static Double getRoot(List<Double> factors, int degree){
+        Double result;
+        Double argument = -1000.0;
+        while(argument <= 1000.0){
+
+            result = evaluate(factors, argument);
+            result = setPrecision(result);
+            printResult(result, argument);
+
+            if(result == 0.0){
+                return setPrecision(argument);
+            }
+            argument+=0.1;
+        }
+        return null;
+    }
 
     private static int getDegree() {
 
@@ -46,6 +63,10 @@ public class CalculateRoots {
         result = factors.get(0) + factors.get(1) * value + factors.get(2) * pow(value, 2);
 
         return result;
+    }
+
+    private static Double setPrecision(Double argument){
+        return Double.parseDouble(String.format("%.5f", argument));
     }
 
 }
